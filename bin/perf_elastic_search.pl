f45e019018f43755e7da14d09adc5082c219e702
Jerome Gagnon <jerome@wajam.com>
2013-09-05 14:05:20 -0400

    missing comma

--- bin/perf_elastic_search.pl
+++ bin/perf_elastic_search.pl
@@ -159,7 +159,7 @@ sub parse_stats {
         "indices.size $node->{indices}{store}{size_in_bytes}",
         "indices.docs $node->{indices}{docs}{count}",
         # Throttling
-        "indices.throttle.total_ms $node->{indices}{store}{throttle_time_in_millis}"
+        "indices.throttle.total_ms $node->{indices}{store}{throttle_time_in_millis}",
         # Indexing
         "indices.indexing.total $node->{indices}{indexing}{index_total}",
         "indices.indexing.total_ms $node->{indices}{indexing}{index_time_in_millis}",
@@ -183,7 +183,7 @@ sub parse_stats {
         "indices.cache.filter_evictions $node->{indices}{filter_cache}{evictions}",
         "indices.cache.filter_size $node->{indices}{filter_cache}{memory_size_in_bytes}",
         # Merges
-        "indices.merges.total $node->{indices}{merges}{total}"
+        "indices.merges.total $node->{indices}{merges}{total}",
         "indices.merges.total_docs $node->{indices}{merges}{total_docs}",
         "indices.merges.total_size $node->{indices}{merges}{total_size_in_bytes}",
         "indices.merges.total_ms $node->{indices}{merges}{total_time_in_millis}",
